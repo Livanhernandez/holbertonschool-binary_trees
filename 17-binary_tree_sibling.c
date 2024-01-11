0764fac3954874a40d0ba2ccaa8197888e50517c
Livanhernandez <livanhernandez9@gmail.com>
2024-01-11 13:56:38 -0400

    fixed description problem with sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_siblings - Finds the siblings of a node in a binary tree
+ * binary_tree_sibling - Finds the siblings of a node in a binary tree
  * @node: Pointer to the node for which to find the sibling
  * Return: Pointer for the sibling node, or NULL if no sibling
  */
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 
 	if (node->parent->left == node)
-		return(node->parent->right);
+		return (node->parent->right);
 	else
 		return (node->parent->left);
 }
